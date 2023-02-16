7a75a70e99cdc528a5ffd2b4f4ff5bac0f99c378
Gustavo Mendonça <alf4r6@gmail.com>
2023-02-16 18:05:52 -0300

    feat: created seeding db

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -46,4 +46,6 @@ export class Account {
       console.log(error);
     }
   }
+
+  
 }
---
+++ src/db/seed.ts
@@ -0,0 +1,39 @@
+import { prisma } from "../lib/prisma"; 
+import { centsToIntegers } from "../utils/centsToIntegers";
+
+const firstAccountNumber = 286359;
+const secondAccountNumber = 758016;
+
+async function run() {
+  await prisma.account.deleteMany();
+  await prisma.transaction.deleteMany();
+
+  // create default users "Jonh Doe" with 100$ and "Jane Doe" with 560$
+
+  await Promise.all([
+    await prisma.account.create({
+      data: {
+        firstName: "Jonh",
+        lastName: "Doe",
+        amount: centsToIntegers(100),
+        numberAccont: firstAccountNumber
+      }
+    }),
+
+    await prisma.account.create({
+      data: {
+        firstName: "Jane",
+        lastName: "Doe",
+        amount: centsToIntegers(560),
+        numberAccont: secondAccountNumber
+      }
+    })
+  ]);
+}
+
+try {
+  run();
+} catch (error) {
+  console.log(error);
+  
+}
